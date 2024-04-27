1aa61a40bfd4050a468028f65fe6072f51e227ea
Nicolas R F Perez <pepitoperezpunk@gmail.com>
2024-04-27 19:14:37 -0300

    Control structures if elif else to match case

--- errores.py
+++ errores.py
@@ -10,3 +10,13 @@ def validarIngresoEntero(leyenda=" "):
       print("Por favor debe ingresar un numero entero.")
       decorar()
   return entero
+
+def validarIngresoPPT(leyenda=" "):
+  while True:
+    try:
+      entero =int(input(leyenda))
+      break
+    except ValueError:
+      print("Por favor debe ingresar una opcion valida.")
+      decorar()
+  return entero
